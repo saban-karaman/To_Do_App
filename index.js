bf582b664a89effb0f1898b40982107058240dd5
Saban Karaman <karaman.saban@gmail.com>
2021-07-18 15:40:38 +0300

    my todo list start

---
+++ index.js
@@ -0,0 +1,29 @@
+// var t1 = document.getElementById("t1")
+// var t2 = document.querySelector("#t2")
+// var t3 = document.querySelector("#t3")
+// var t4 = document.querySelector("#t4")
+// var t5 = document.querySelector("#t5")
+// var t6 = document.querySelector("#t6")
+// var t7 = document.querySelector("#t7")
+
+
+
+
+// function overlie(name) {
+//     name.style.color = "red";
+//     name.style.textDecoration = "line-through";
+// }
+
+// function clr(name) {
+//     if (name.checked = false) {
+//     console.log("neden")
+//     name.style.color = "blue";
+//     name.style.textDecoration = "none"; 
+//     }
+// }
+
+// function myFun(name) {
+//     clr(name);
+//     overlie(name);
+    
+// }
